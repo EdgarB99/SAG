66f5440b95fb77e3567ff13746f4b257e9ac1bd4
EdgarB99 <black11.eb12@gmail.com>
2023-07-29 14:51:10 -0600

    cambio en appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { ContenidoDieta } from './contenido-dieta/entities/contenido-dieta.entit
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.HOSTDEV,
-      port: 7714,
+      port: 3000,
       username: process.env.DBUSERNAME,
       password: process.env.PASSDEV,
       database: process.env.DBNAMEDEV,
